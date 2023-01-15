452b8e58abb5edb8cea49594dc29e04f92ecbd73
Imad Bin Asad <imadbinasad1@gmail.com>
2023-01-15 20:01:11 +0530

    adjust text

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -8,7 +8,7 @@ const Benefits = () => {
          <div class="row">
            
             <div className="heading_main text_align_center" style={{paddingTop:"70px"}}>
-						   <h2>Benefits</h2>
+						   <h2 style={{color:"#E5E4E2"}}>Benefits</h2>
                         </div>
          </div>
 
@@ -19,42 +19,41 @@ const Benefits = () => {
          
          <div className="col-md-4 col-sm-6">
             <div id="ho_color" class="service_box">
-            <i class="fa fa-4x fa-trophy" style={{marginTop: "30px"}} aria-hidden="true"></i>
-                <h3 style={{marginLeft:"0px", marginTop:"20px"}}>International Competition</h3> 
+            <i class="fa fa-4x fa-trophy" style={{marginTop: "30px",color:"#E5E4E2"}} aria-hidden="true" ></i>
+                <h3 style={{marginLeft:"0px", marginTop:"20px",color:"#E5E4E2"}}>International Competitions</h3> 
                {/* <p style={{marginLeft:"15px"}}>{object.text}</p> */}
             </div>
          </div>
          
-
          <div className="col-md-4 col-sm-6">
             <div id="ho_color" class="service_box">
-            <i class="fa fa-4x fa-users" style={{marginTop: "30px"}} aria-hidden="true"></i>
-                <h3 style={{marginLeft:"0px", marginTop:"20px"}}>Interaction with Professional</h3> 
+            <i class="fa fa-4x fa-users" style={{marginTop: "30px",color:"#E5E4E2"}} aria-hidden="true"></i>
+                <h3 style={{marginLeft:"0px", marginTop:"20px",color:"#E5E4E2"}}>Interaction with Professionals</h3> 
                {/* <p style={{marginLeft:"15px"}}>{object.text}</p> */}
             </div>
          </div><div className="col-md-4 col-sm-6">
             <div id="ho_color" class="service_box">
-            <i class="fa fa-4x fa-bell" style={{marginTop: "30px"}} aria-hidden="true"></i> 
-                <h3 style={{marginLeft:"0px", marginTop:"20px"}}>Awareness about technology</h3> 
+            <i class="fa fa-4x fa-bell" style={{marginTop: "30px",color:"#E5E4E2"}} aria-hidden="true"></i> 
+                <h3 style={{marginLeft:"0px", marginTop:"20px",color:"#E5E4E2"}}>Awareness of Technology</h3> 
                {/* <p style={{marginLeft:"15px"}}>{object.text}</p> */}
             </div>
          </div><div className="col-md-4 col-sm-6">
             <div id="ho_color" class="service_box">
-            <i class="fas fa-4x fa-tasks" style={{marginTop: "30px"}} aria-hidden="true"></i>
-                <h3 style={{marginLeft:"0px", marginTop:"20px"}}>Free Workshops</h3> 
+            <i class="fas fa-4x fa-tasks" style={{marginTop: "30px",color:"#E5E4E2"}} aria-hidden="true"></i>
+                <h3 style={{marginLeft:"0px", marginTop:"20px",color:"#E5E4E2"}}>Tech Workshops</h3> 
                {/* <p style={{marginLeft:"15px"}}>{object.text}</p> */}
             </div>
          </div><div className="col-md-4 col-sm-6">
             <div id="ho_color" class="service_box">
-            <i class="fa fa-4x fa-desktop" style={{marginTop: "30px"}} aria-hidden="true"></i>
-                <h3 style={{marginLeft:"0px", marginTop:"20px"}}>Opportunity for projects</h3> 
+            <i class="fa fa-4x fa-desktop" style={{marginTop: "30px",color:"#E5E4E2"}} aria-hidden="true"></i>
+                <h3 style={{marginLeft:"0px", marginTop:"20px",color:"#E5E4E2"}}>Opportunity for projects</h3> 
                {/* <p style={{marginLeft:"15px"}}>{object.text}</p> */}
             </div>
          </div>
          <div className="col-md-4 col-sm-6">
             <div id="ho_color" class="service_box">
-            <i class="fa fa-4x fa-thumbs-up" style={{marginTop: "30px"}} aria-hidden="true"></i>
-                <h3 style={{marginLeft:"0px", marginTop:"20px"}}>Great Team :)</h3> 
+            <i class="fa fa-4x fa-thumbs-up" style={{marginTop: "30px",color:"#E5E4E2"}} aria-hidden="true"></i>
+                <h3 style={{marginLeft:"0px", marginTop:"20px",color:"#E5E4E2"}}>Awesome Team :)</h3> 
                {/* <p style={{marginLeft:"15px"}}>{object.text}</p> */}
             </div>
          </div>
