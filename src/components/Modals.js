c94f7e24a04a16deb9a905a666a198ee0b6a5de6
Mohammad Ali <mohammadali200314@gmail.com>
2024-08-13 20:56:49 +0530

    app link

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,84 +1,102 @@
-import React from 'react'
-import '../assets/ComponentDesign/Modal.css';
+import React from "react";
+import "../assets/ComponentDesign/Modal.css";
 const Modals = (props) => {
-
- 
   return (
-
-
-
-
-
- <div>
-
-
-
-{/* <!-- Modal --> */}
-<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog"  role="document">
-    <div class="modal-content darkBG"  >
-      <div class="modal-header " >
-        <h5 class="modal-title" id="exampleModalLongTitle" style={{color:"white"}}>{props.name}</h5>
-        <button type="button" onClick={props.hide} class="close" style={{color:"white"}} data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <img src={props.img} className="img-fluid"alt="sui" style={{marginBottom:"14px", borderRadius:"20px"}} />
-      <p style={{fontSize:"12px", color:"white"}}>Completion Date: {props.date}</p>
-      <p style={{marginLeft:"13px", marginRight:"13px", color:"white", textAlign: "justify"}}>{props.description}</p>
-      {
-     props.teamMembers? <h2 style={{fontWeight: "bold", marginLeft:"13px", color:"white"}}>Team</h2>:''
-    }
-        {props.teamMembers?.map((object)=>{
-         return <li style={{listStyleType: "circle", marginLeft: "13px", color:"white"}}> {object.member}</li>;
-    })}
-      </div>
-      <div class="modal-footer">
-       
-        <button type="button" aria-label="Close" data-dismiss="modal" class="btn8 success" onClick={props.hide}>Close</button>
+    <div>
+      {/* <!-- Modal --> */}
+      <div
+        class="modal fade"
+        id="exampleModalLong"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLongTitle"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content darkBG">
+            <div class="modal-header ">
+              <h5
+                class="modal-title"
+                id="exampleModalLongTitle"
+                style={{ color: "white" }}
+              >
+                {props.name}
+              </h5>
+              <button
+                type="button"
+                onClick={props.hide}
+                class="close"
+                style={{ color: "white" }}
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <img
+                src={props.img}
+                className="img-fluid"
+                alt="sui"
+                style={{ marginBottom: "14px", borderRadius: "20px" }}
+              />
+              <p style={{ fontSize: "12px", color: "white" }}>
+                Completion Date: {props.date}
+              </p>
+              <p
+                style={{
+                  marginLeft: "13px",
+                  marginRight: "13px",
+                  color: "white",
+                  textAlign: "justify",
+                }}
+              >
+                {props.description}
+              </p>
+              {props.teamMembers ? (
+                <h2
+                  style={{
+                    fontWeight: "bold",
+                    marginLeft: "13px",
+                    color: "white",
+                  }}
+                >
+                  Team
+                </h2>
+              ) : (
+                ""
+              )}
+              {props.teamMembers?.map((object) => {
+                return (
+                  <li
+                    style={{
+                      listStyleType: "circle",
+                      marginLeft: "13px",
+                      color: "white",
+                    }}
+                  >
+                    {" "}
+                    {object.member}
+                  </li>
+                );
+              })}
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                aria-label="Close"
+                data-dismiss="modal"
+                class="btn8 success"
+                onClick={props.hide}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
-
-  )
-}
-
-export default Modals
+export default Modals;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,167 +1,211 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from "react";
 import Particles, { ISourceOptions } from "react-tsparticles";
-import Footer from '../components/Footer';
-import ImageSlider from '../components/ImageSlider';
-import FAQ from '../components/FAQ';
+import Footer from "../components/Footer";
+import ImageSlider from "../components/ImageSlider";
+import FAQ from "../components/FAQ";
 // import './css/bootstrap.min.css';
 // import './css/pogo-slider.min.css';
-import './css/style.css';
+import "./css/style.css";
 // import './css/custom.css';
 // import './css/responsive.css';
-import LoadingSpinner from '../components/LoadingSpinner';
+import LoadingSpinner from "../components/LoadingSpinner";
 // import './css/responsiveslides.css';
 import { Link } from "react-router-dom";
-import Test from '../components/Test';
-import Benefits from '../components/Benefits';
-import Team from '../components/Team';
-import Achievements from '../components/Achievements';
+import Test from "../components/Test";
+import Benefits from "../components/Benefits";
+import Team from "../components/Team";
+import Achievements from "../components/Achievements";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Upcoming from "../pages/Upcoming"
-import '../assets/ComponentDesign/Navbar.css';
-import '../assets/ComponentDesign/Hover.css';
-import '../assets/ComponentDesign/VerceraButton.css';
+import Upcoming from "../pages/Upcoming";
+import "../assets/ComponentDesign/Navbar.css";
+import "../assets/ComponentDesign/Hover.css";
+import "../assets/ComponentDesign/VerceraButton.css";
 
 // import ParticleBackground from 'react-particle-backgrounds'
 
+import Location from "../components/Location";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
-
-
-
-
-import Location from '../components/Location';
-import { AnimationOnScroll } from 'react-animation-on-scroll';
-
-import Modals from '../components/Modals';
-import Cards from '../components/Cards';
-import emailjs from '@emailjs/browser';
+import Modals from "../components/Modals";
+import Cards from "../components/Cards";
+import emailjs from "@emailjs/browser";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 // import PhotoGallery from '../components/PhotoGallery'
-import Introduction from '../components/Introduction';
+import Introduction from "../components/Introduction";
 
 // import { grey, pink } from '@material-ui/core/colors';
 
 // import { grey, pink, red } from '@material-ui/core/colors';
-import content from "../assets/cms/content.json"
+import content from "../assets/cms/content.json";
 // import PhotoGallery from '../components/PhotoGallery';
 const Home = () => {
-
-
-
   const myStyle = {
     backgroundImage:
       "url('https://res.cloudinary.com/amuroboclub/image/upload/v1673816613/2022-23_website_react/About/background2.jpg')",
     opacity: 0.95,
     // backgroundSize: 'cover',
-    backgroundRepeat: 'repeat',
+    backgroundRepeat: "repeat",
   };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   return (
-
     <div style={myStyle}>
+      <nav class="navbar navbar-expand-lg navbar-dark elegant-color ">
+        <a class="navbar-brand" href="#">
+          <Link to="/">
+            <img
+              src={require("../assets/images/Vlogo.png")}
+              style={{ borderRadius: "10px", userSelect: "none" }}
+              alt="image"
+            />
+          </Link>
+        </a>
 
-      <nav class="navbar navbar-expand-lg navbar-dark elegant-color " >
-
-        <a class="navbar-brand" href="#"><Link to="/"><img src={require('../assets/images/Vlogo.png')} style={{ borderRadius: "10px", userSelect: "none" }} alt="image" /></Link></a>
-
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
-          aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
-
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#basicExampleNav"
+          aria-controls="basicExampleNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <i class="fa-solid fa-bars" style={{ color: "white" }}></i>
         </button>
 
-
         <div class="collapse navbar-collapse" id="basicExampleNav">
-
-
-          <ul class="navbar-nav mr-auto" >
+          <ul class="navbar-nav mr-auto">
             <li class="nav-item" style={{ fontSize: "13px" }}>
-              <Link to='/' > <a class="nav-link hover-underline-animation" href="#"><span style={{ color: "white", userSelect: "none" }}>Home
-              </span></a></Link>
+              <Link to="/">
+                {" "}
+                <a class="nav-link hover-underline-animation" href="#">
+                  <span style={{ color: "white", userSelect: "none" }}>
+                    Home
+                  </span>
+                </a>
+              </Link>
             </li>
             <li class="nav-item" style={{ fontSize: "13px" }}>
-              <Link to='/notices' > <a class="nav-link hover-underline-animation" href="#"><span style={{ color: "white", userSelect: "none" }}>Notices
-              </span></a></Link>
+              <Link to="/notices">
+                {" "}
+                <a class="nav-link hover-underline-animation" href="#">
+                  <span style={{ color: "white", userSelect: "none" }}>
+                    Notices
+                  </span>
+                </a>
+              </Link>
             </li>
 
             <li class="nav-item" style={{ fontSize: "13px" }}>
-              <Link to='/blogs' > <a class="nav-link hover-underline-animation" href="#"><span style={{ color: "white", userSelect: "none" }}>Blogs
-              </span></a></Link>
+              <Link to="/blogs">
+                {" "}
+                <a class="nav-link hover-underline-animation" href="#">
+                  <span style={{ color: "white", userSelect: "none" }}>
+                    Blogs
+                  </span>
+                </a>
+              </Link>
             </li>
-
-
-
 
             <li class="nav-item" style={{ fontSize: "13px" }}>
-              <Link to='/upcomingprojects' > <a class="nav-link hover-underline-animation" href="#"><span style={{ color: "white", userSelect: "none" }}>Project Recruitment
-              </span></a></Link>
-            </li>
-
-
-
-
-            <li class="nav-item active" style={{ fontSize: "13px" }}>
-              <Link to='/projects'><a class="nav-link hover-underline-animation" href="/projects" ><span style={{ color: "white", userSelect: "none" }}>Projects
-              </span>
-
-              </a></Link>
-            </li>
-            <li style={{ fontSize: "13px", }}>
-              <a class="nav-link hover-underline-animation" target="blank" href="https://drive.google.com/file/d/1sML0h9K8s9u33vSQpi0LGo_mfQyqLxo_/view?usp=sharing"><span style={{ color: "white", userSelect: "none" }}>Newsletter
-              </span></a>
-            </li>
-
-
-
-            <li class="nav-item active" style={{ fontSize: "13px", color: "white" }}>
-              <a class="nav-link hover-underline-animation" href="#"><AnchorLink href='#team' style={{ color: "white", userSelect: "none" }}>Team
-              </AnchorLink></a>
+              <Link to="/upcomingprojects">
+                {" "}
+                <a class="nav-link hover-underline-animation" href="#">
+                  <span style={{ color: "white", userSelect: "none" }}>
+                    Project Recruitment
+                  </span>
+                </a>
+              </Link>
             </li>
 
             <li class="nav-item active" style={{ fontSize: "13px" }}>
-              <a class="nav-link hover-underline-animation"href="https://amu-sat.github.io/"  ><span style={{ color: "white", userSelect: "none" }}>AmuSat
-              </span>
-
+              <Link to="/projects">
+                <a class="nav-link hover-underline-animation" href="/projects">
+                  <span style={{ color: "white", userSelect: "none" }}>
+                    Projects
+                  </span>
+                </a>
+              </Link>
+            </li>
+            <li style={{ fontSize: "13px" }}>
+              <a
+                class="nav-link hover-underline-animation"
+                target="blank"
+                href="https://drive.google.com/file/d/1sML0h9K8s9u33vSQpi0LGo_mfQyqLxo_/view?usp=sharing"
+              >
+                <span style={{ color: "white", userSelect: "none" }}>
+                  Newsletter
+                </span>
               </a>
             </li>
 
-            <li class="nav-item active" style={{ fontSize: "14px", color: "white", userSelect: "none" }}>
-              <a class="nav-link hover-underline-animation" href="#" data-backdrop="false" type="button" data-toggle="modal" data-target="#modalRegular">Location
+            <li
+              class="nav-item active"
+              style={{ fontSize: "13px", color: "white" }}
+            >
+              <a class="nav-link hover-underline-animation" href="#">
+                <AnchorLink
+                  href="#team"
+                  style={{ color: "white", userSelect: "none" }}
+                >
+                  Team
+                </AnchorLink>
               </a>
             </li>
 
-            <li style={{ fontSize: "14px", }}>
-              <div><a class="nav-link hover-underline-animation " href="mailto:amuroboclub@gmail.com"><span style={{ color: "white", userSelect: "none" }}>Email
-              </span></a></div>
+            <li class="nav-item active" style={{ fontSize: "13px" }}>
+              <a
+                class="nav-link hover-underline-animation"
+                href="https://amu-sat.github.io/"
+              >
+                <span style={{ color: "white", userSelect: "none" }}>
+                  AmuSat
+                </span>
+              </a>
+            </li>
+
+            <li
+              class="nav-item active"
+              style={{ fontSize: "14px", color: "white", userSelect: "none" }}
+            >
+              <a
+                class="nav-link hover-underline-animation"
+                href="#"
+                data-backdrop="false"
+                type="button"
+                data-toggle="modal"
+                data-target="#modalRegular"
+              >
+                Location
+              </a>
+            </li>
+
+            <li style={{ fontSize: "14px" }}>
+              <div>
+                <a
+                  class="nav-link hover-underline-animation "
+                  href="mailto:amuroboclub@gmail.com"
+                >
+                  <span style={{ color: "white", userSelect: "none" }}>
+                    Email
+                  </span>
+                </a>
+              </div>
             </li>
             <li class="nav-item" style={{ fontSize: "14px" }}>
-              <a class="nav-link hover-underline-animation" href="#faq-div"><span style={{ color: "white", userSelect: "none" }}>FAQ
-              </span></a>
+              <a class="nav-link hover-underline-animation" href="#faq-div">
+                <span style={{ color: "white", userSelect: "none" }}>FAQ</span>
+              </a>
             </li>
-            <li style={{ fontSize: "14px" }}><Link to='/contributors' > <a class="nav-link hover-underline-animation" href="#"><span style={{ color: "white", userSelect: "none", }}>Contributors</span></a></Link></li>
+            <li style={{ fontSize: "14px" }}>
+              <Link to="/contributors">
+                {" "}
+                <a class="nav-link hover-underline-animation" href="#">
+                  <span style={{ color: "white", userSelect: "none" }}>
+                    Contributors
+                  </span>
+                </a>
+              </Link>
+            </li>
 
             {/* <li style={{fontSize:"15px"}}><Link to='/news' > <a class="nav-link hover-underline-animation"  href="#"><span style={{color:"white", userSelect: "none", }}>News<i class="fa-solid fa-radio" style={{marginLeft:"4px"}}></i></span></a></Link></li> */}
             {/* <li class="nav-item dropdown" style={{fontSize:"15px",}}>
@@ -175,102 +219,96 @@ const Home = () => {
       <div> </div> 
         </div>
       </li> */}
-
           </ul>
-
-
-
         </div>
-
-
-      </nav >
-
-
+      </nav>
 
       <div style={{ backgroundColor: "whitesmoke", margin: "20px" }}>
-        <marquee behavior="" direction="" style={{ fontSize: "1.2rem" }}><Link to='/blogs'>Check out our Blogs Page</Link></marquee>
+        <marquee>
+          <a
+            href="https://drive.google.com/file/d/1RFu5NtxDXvg54ka9GJcrxYiexZaR1wXk/view?usp=sharing"
+            download
+          >
+            Download Our Club's Application
+          </a>
+        </marquee>
       </div>
-
-
-
-
-
-
-
-
-
-
 
       {/* <!-- section --> */}
 
-      < Introduction />
+      <Introduction />
 
-
-
-
-      <AnimationOnScroll animateIn="animate__fadeInRight" animateOnce="true" offset={15} duration="0.5">
+      <AnimationOnScroll
+        animateIn="animate__fadeInRight"
+        animateOnce="true"
+        offset={15}
+        duration="0.5"
+      >
         <div className="section margin-top_3" style={{ paddingTop: "0px" }}>
           <div className="container">
             <div className="row">
               <div className="col-md-6">
                 <div className="full" style={{ paddingTop: "130px" }}>
-                  <img src={content[0].aboutImages[1].image} alt="#" style={{ height: "460px", borderRadius: "40px", userSelect: "none" }} />
+                  <img
+                    src={content[0].aboutImages[1].image}
+                    alt="#"
+                    style={{
+                      height: "460px",
+                      borderRadius: "40px",
+                      userSelect: "none",
+                    }}
+                  />
                 </div>
               </div>
               <div className="col-md-6 layout_padding_2">
                 <div className="full">
                   <div className="heading_main text_align_center">
-                    <h2 style={{ color: "#E5E4E2" }}>Why Join Us
-                    </h2>
+                    <h2 style={{ color: "#E5E4E2" }}>Why Join Us</h2>
                   </div>
-                  <p style={{ color: "#E5E4E2", textAlign: "justify", fontSize: "15px" }} >{content[0].whyjoin}</p>
-
+                  <p
+                    style={{
+                      color: "#E5E4E2",
+                      textAlign: "justify",
+                      fontSize: "15px",
+                    }}
+                  >
+                    {content[0].whyjoin}
+                  </p>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
-
       </AnimationOnScroll>
-      <AnimationOnScroll animateIn="animate__fadeInLeft" animateOnce="true" offset={15} duration="0.5">
-        <div className="heading_main text_align_center" >
+      <AnimationOnScroll
+        animateIn="animate__fadeInLeft"
+        animateOnce="true"
+        offset={15}
+        duration="0.5"
+      >
+        <div className="heading_main text_align_center">
           <h2 style={{ color: "#E5E4E2" }}>What Makes Us Different</h2>
         </div>
         <div className="section margin-top_50">
           <div className="container">
-            <div className="row "><p style={{ color: "#E5E4E2", textAlign: "justify", marginLeft: "10px", marginRight: "10px", fontSize: "20px", fontFamily: "Lucida Handwriting", fontStyle: "italic" }}>{content[0].whatMakesUsDifferent}...</p>
-            </div></div></div></AnimationOnScroll>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            <div className="row ">
+              <p
+                style={{
+                  color: "#E5E4E2",
+                  textAlign: "justify",
+                  marginLeft: "10px",
+                  marginRight: "10px",
+                  fontSize: "20px",
+                  fontFamily: "Lucida Handwriting",
+                  fontStyle: "italic",
+                }}
+              >
+                {content[0].whatMakesUsDifferent}...
+              </p>
+            </div>
+          </div>
+        </div>
+      </AnimationOnScroll>
 
       {/* <AnimationOnScroll animateIn="animate__fadeInLeft" animateOnce="true" duration="0.7">
         <Achievements /></AnimationOnScroll>
@@ -283,11 +321,7 @@ const Home = () => {
         <Cards />
       </AnimationOnScroll> */}
 
-
-
       <Benefits />
-
-
 
       {/* 
       <div class="section margin-top_50 silver_bg" id='benefits'>
@@ -301,41 +335,21 @@ const Home = () => {
         </div>
       </div> */}
 
-
-
-
-
-
-
-
-
-
-
       <Team />
 
       <Location />
 
-
-
-
-
-
-
-
-
       <ImageSlider />
 
-      <div id='faq-div'>
+      <div id="faq-div">
         <FAQ />
       </div>
 
       <div style={{ marginTop: "70px" }}>
-        <Footer /></div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-
-
-    </div >
-  )
-}
-
-export default Home
+export default Home;
